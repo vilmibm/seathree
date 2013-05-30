77862c0c1fbf4d360bb169ea891ca252003bbe42
Nathaniel Smith <nathaniel@puppetlabs.com>
2013-05-29 22:52:48 -0400

    docstrings, TODOs

--- daemon.py
+++ daemon.py
@@ -2,7 +2,7 @@ import json
 import re
 
 from twython import Twython, TwythonStreamer
-from apiclient.discovery import build # Google's poorly named thing
+from apiclient.discovery import build # Google's poorly named thing. It cannot be installed into a virtualenv; be warned.
 
 SECRETS_FILE = 'secrets.json'
 
@@ -29,13 +29,19 @@ class TranslationStreamer(TwythonStreamer):
         self.google_client = client
 
     def translate(self, text, source="es", target="en"):
+        """
+        Send text off to google to be translated. Can take a long time
+        and sometimes fails. Returns `text` in language `source`
+        translated into language `target`.
+        """
+        # TODO this fails with BadStatus sometimes. It tends to work upon retry. Write retry code.
         return self.google_client.translations().list(source=source, target=target, q=text).execute()
 
     def is_original_tweet(self, data):
         """
         Given some data from the streaming API, determine if it is an
         original tweet from one of the authed account's
-        followings. Returns True or False
+        followings. Returns True or False.
         """
         # Favorites do not come through. Non-tweets lack a text property,
         # it seems (ie new followings).
@@ -56,10 +62,19 @@ class TranslationStreamer(TwythonStreamer):
         }
 
     def mksigil(self, c):
+        """
+        Create a sigil used to replace Twitter symbols like
+        @replies. Necessary to preserve symbols across trips to
+        google. Also cuts down on google character usage. Got to save
+        dollars.
+
+        Takes an int, returns a string like `xz0`.
+        """
         return 'xz%d' % c
 
     def mark_sigils(self, raw_text):
-        """Replace special forms within a tweet with ordered sigils so
+        """
+        Replace special forms within a tweet with ordered sigils so
         they can be restored later. For example,
 
         Este cerveza esta sabrosa #cerveza http://bit.ly/foo @friend
@@ -85,6 +100,10 @@ class TranslationStreamer(TwythonStreamer):
         return marked_text, symbols
 
     def restore_sigils(self, marked_text, symbols):
+        """
+        Restore a list of symbols to their placeholders. Accepts the
+        output of `mark_sigils`.
+        """
         counter = 0
         unmarked_text = marked_text
         for symbol in symbols:
@@ -94,6 +113,12 @@ class TranslationStreamer(TwythonStreamer):
         return unmarked_text
 
     def tweet(self, details):
+        """
+        Given data about a tweet, including its `translated_text`,
+        create one or multiple tweets and send them to twitter.
+
+        Returns None.
+        """
         text = details['translated_text']
         screen_name = details['user']['screen_name']
         naive_tweet = "@{screen_name}: {text}".format(screen_name=screen_name, text=text)
@@ -112,12 +137,22 @@ class TranslationStreamer(TwythonStreamer):
                 current_tweet = ''
         tweets[len(tweets)-1] = tweets[len(tweets)-1][:-3] # shave off final ellipsis
         for tweet in tweets:
+            # TODO could a tight loop cause rate-limit issues? Might
+            # want to sleep here.
             self.twitter_client.update_status(tweet)
 
     def listen(self):
+        """ Start listening to the Twitter home timeline. """
         self.user(**{'with':'following'})
 
     def on_success(self, data):
+        """
+        Data handler for Twitter stream. Handles all our business
+        logic for getting tweets translated and sent back out by our
+        bot.
+        """
+        # TODO spawn a thread to do this. Google hangs for so long
+        # we'll get a huge backlog of tweets.
         if not self.is_original_tweet(data):
             return
         details = self.extract_details(data)
@@ -129,6 +164,11 @@ class TranslationStreamer(TwythonStreamer):
         self.tweet(details)
 
     def on_error(self, status_code, data):
+        """
+        Error handler for Twitter stream. I haven't actually seen any
+        errors yet so I'm not sure what the most useful thing to do
+        here is. Printing should be sufficient, for now.
+        """
         print status_code, data
 
 
@@ -138,6 +178,7 @@ if __name__ == '__main__':
     print "Connecting twitter client"
     twitter_client = Twython(*get_twitter_auth())
     home_stream.set_twitter_client(twitter_client)
+
     print "Connecting Google client"
     home_stream.set_google_client(get_google_client())
 
