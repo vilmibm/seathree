719b23f493bdf81f9adf00a7581f57907046a22e
Nathaniel Smith <nathaniel@puppetlabs.com>
2014-01-11 11:38:38 -0500

    Add stubbed JS/CSS

---
+++ resources/web/app/scripts/app.js
@@ -0,0 +1,13 @@
+(function (ng) {
+
+  ng.module('SeaThree', []).
+
+  controller('LanguageCtrl', function ($scope) {
+
+  }).
+
+  controller('TweetsCtrl', function ($http, $scope) {
+  });
+
+
+})(angular);
