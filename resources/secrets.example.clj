2593ef112322564e99ebd8427e7ca2f58504c14d
Nathaniel Smith <nathaniel@puppetlabs.com>
2014-01-11 11:51:43 -0500

    Clean up repo

---
+++ resources/secrets.example.clj
@@ -0,0 +1,6 @@
+{:twitter {:oauth {:consumer-key        ""
+                   :consumer-secret     ""
+                   :access-token        ""
+                   :access-token-secret ""}}
+ :google {:key ""}
+ :redis  {:port 6379 :host "localhost"}}
