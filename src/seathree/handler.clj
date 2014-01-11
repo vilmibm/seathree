14b944dfcc3fc38036193393a04c3dc81623ef8e
Nathaniel Smith <nathaniel@puppetlabs.com>
2014-01-11 11:47:40 -0500

    Licensing.

--- src/seathree/handler.clj
+++ src/seathree/handler.clj
@@ -1,3 +1,19 @@
+; SeaThree, Realtime Twitter Translations
+; Copyright (C) 2014 Nathaniel Smith and Benjamin Valentine
+;
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU Affero General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU Affero General Public License for more details.
+;
+; You should have received a copy of the GNU Affero General Public License
+; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (ns seathree.handler
   (:gen-class)
   (:require [clojure.tools.nrepl.server :as nrsrv         ]
