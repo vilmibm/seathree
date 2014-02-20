6e3bc3dc9cc1d75544ff0a16712c69ebc21beac3
Nathaniel Smith <nathaniel@puppetlabs.com>
2014-02-19 20:23:08 -0500

    Clean up some JS and start on adding users

--- resources/web/app/scripts/app.js
+++ resources/web/app/scripts/app.js
@@ -1,62 +1,78 @@
 (function (ng, _) {
   var SEATHREEURL   = 'http://localhost:8888/tweets-for-many',
-      FETCHINTERVAL = 30000;
+      FETCHINTERVAL = 30000,
+      List,
+      defaultLists,
+      defaultUsers,
+      findUser;
+
+  List = function (name, usernames) {
+    this.name = name;
+    this.usernames = usernames;
+    this.selected = false;
+    this.tweets = [];
+  };
+
+  findUser = function (users, username) {
+    return _(users).find(function (user) {
+      return user.username === username;
+    });
+  };
+
+
+  defaultUsers = [{
+    username: 'GobiernoDF',
+    src: 'es',
+    tgt: 'en',
+    tweets: []
+  }, {
+    username: 'VertigoPolitico',
+    src: 'es',
+    tgt: 'en',
+    tweets: []
+  }, {
+    username: 'bennnyv',
+    src: 'en',
+    tgt: 'es',
+    tweets: []
+  }, {
+    username: 'nate_smith',
+    src:'en',
+    tgt:'es',
+    tweets: []
+  }];
+
+  defaultLists = [
+    new List('Mexico', ['GobiernoDF', 'VertigoPolitico']),
+    new List('United States', ['nate_smith', 'bennnyv']),
+    new List('Organizations', ['VertigoPolitico']),
+    new List('My List', [])
+  ];
+
+  defaultLists.unshift(new List('All',
+                                _.chain(defaultLists)
+                                .pluck('usernames')
+                                .flatten()
+                                .unique()
+                                .value()));
 
   ng.module('SeaThree', ['ngSanitize'])
   .controller('TweetsCtrl', function ($scope, $http, $interval) {
-    var findUser,
-        List,
-        fetch;
-    List = function (name, usernames) {
-      this.name = name;
-      this.usernames = usernames;
-      this.selected = false;
-      this.tweets = [];
-    };
+    var fetch,
+        addUserAt;
 
-    $scope.lists = [
-      new List('Mexico', ['GobiernoDF', 'VertigoPolitico']),
-      new List('United States', ['nate_smith', 'bennnyv']),
-      new List('Organizations', ['VertigoPolitico']),
-      new List('My List', [])
-    ];
-    $scope.lists.unshift(new List('All', _.chain($scope.lists)
-                                         .pluck('usernames')
-                                         .flatten()
-                                         .unique()
-                                         .value()));
+    $scope.lists = defaultLists;
 
-    $scope.users = [{
-      username: 'GobiernoDF',
-      src: 'es',
-      tgt: 'en',
-      tweets: []
-    }, {
-      username: 'VertigoPolitico',
-      src: 'es',
-      tgt: 'en',
-      tweets: []
-    }, {
-      username: 'bennnyv',
-      src: 'en',
-      tgt: 'es',
-      tweets: []
-    }, {
-      username: 'nate_smith',
-      src:'en',
-      tgt:'es',
-      tweets: []
-    }];
-
-    findUser = function (users, username) {
-      return _(users).find(function (user) {
-               return user.username === username;
-             });
-    };
+    $scope.users = defaultUsers;
 
     $scope.isSelected = function (list) {return list.selected;};
     $scope.toggleListAt = function (index) {
       $scope.lists[index].selected = !$scope.lists[index].selected;
+    };
+    $scope.addUserAt = function (index) {
+      var list;
+      list = $scope.lists[index];
+      console.log('Adding user');
     };
 
     fetch = function () {
